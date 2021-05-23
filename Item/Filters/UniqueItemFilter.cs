3c3f4eee9d7dc8a80b5367abb53144c5c8848a62
vadash <vadash@gmail.com>
2021-05-23 08:57:00 +0300

    =Fixed uniques name compare

--- Item/Filters/UniqueItemFilter.cs
+++ Item/Filters/UniqueItemFilter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 using ExileCore;
 using ExileCore.PoEMemory.Elements.InventoryElements;
 using EZVendor.Item.Ninja;
@@ -22,10 +24,10 @@ namespace EZVendor.Item.Filters
         {
             try
             {
-                if (ItemModsComponent.UniqueName.Length <= 4)
-                    return Actions.Vendor;
+                if (ItemModsComponent.UniqueName.Length <= 4) return Actions.Vendor;
+                if (ItemModsComponent.UniqueName == @"Hotfooted") return Actions.Vendor; // Hotheaded
                 var garbage = _ninjaProvider.GetCheapUniques();
-                return garbage.Contains(ItemModsComponent.UniqueName)
+                return garbage.Any(name => IsSameName(name, ItemModsComponent.UniqueName))
                     ? Actions.Vendor
                     : Actions.CantDecide;
             }
@@ -33,6 +35,14 @@ namespace EZVendor.Item.Filters
             {
                 return Actions.Keep;
             }
+        }
+
+        private static bool IsSameName(string s1, string s2)
+        {
+            var rgx = new Regex("[^a-zA-Z0-9]");
+            s1 = rgx.Replace(s1, "").ToLower();
+            s2 = rgx.Replace(s2, "").ToLower();
+            return s1 == s2;
         }
     }
 }
